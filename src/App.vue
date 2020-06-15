3073cdfc1d880d2093864499ce10754bab27bd46
Simon Weller <s.meierbeck@gmail.com>
2020-06-15 21:52:05 +0200

    tidy up and bring in html and css from template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoList />
+    <InfoFooter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
+import InfoFooter from './components/InfoFooter'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList: TodoList,
+    InfoFooter: InfoFooter,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
