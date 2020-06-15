7bc4c9f45e2bbd51c56c576fb57818132455384c
Simon Weller <s.meierbeck@gmail.com>
2020-06-15 22:49:04 +0200

    hide main and footer if there are no todos

---
+++ src/components/Todo.vue
@@ -0,0 +1,10 @@
+<template>
+  <li>
+    <div class="view">
+      <input class="toggle" type="checkbox" checked />
+      <label>Taste JavaScript</label>
+      <button class="destroy"></button>
+    </div>
+    <input class="edit" value="Create a TodoMVC template" />
+  </li>
+</template>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,29 +4,14 @@
       <h1>todos</h1>
       <input class="new-todo" placeholder="What needs to be done?" autofocus />
     </header>
-    <section class="main">
+    <section v-if="hasTodos" class="main">
       <input id="toggle-all" class="toggle-all" type="checkbox" />
       <label for="toggle-all">Mark all as complete</label>
       <ul class="todo-list">
-        <li class="completed">
-          <div class="view">
-            <input class="toggle" type="checkbox" checked />
-            <label>Taste JavaScript</label>
-            <button class="destroy"></button>
-          </div>
-          <input class="edit" value="Create a TodoMVC template" />
-        </li>
-        <li>
-          <div class="view">
-            <input class="toggle" type="checkbox" />
-            <label>Buy a unicorn</label>
-            <button class="destroy"></button>
-          </div>
-          <input class="edit" value="Rule the web" />
-        </li>
+        <Todo v-for="todo in todos" :key="todo.id" />
       </ul>
     </section>
-    <footer class="footer">
+    <footer v-if="hasTodos" class="footer">
       <span class="todo-count"><strong>0</strong> item left</span>
       <ul class="filters">
         <li>
@@ -43,3 +28,22 @@
     </footer>
   </section>
 </template>
+
+<script>
+import Todo from "./Todo";
+export default {
+  components: {
+    Todo,
+  },
+  data: () => {
+    return {
+      todos: [],
+    };
+  },
+  computed: {
+    hasTodos: function () {
+      return this.todos.length > 0;
+    },
+  },
+};
+</script>
