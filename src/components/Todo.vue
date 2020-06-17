4ca62f9e79993cf7f7561ba5b7c2bbada7548b77
Simon Weller <s.meierbeck@gmail.com>
2020-06-17 23:18:01 +0200

    Finish Editing mode

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -10,7 +10,14 @@
       <label v-on:dblclick="startEditing">{{ todo.text }}</label>
       <button class="destroy" v-on:click="$emit('destroy')"></button>
     </div>
-    <input class="edit" :value="todo.text" ref="input" />
+    <input
+      class="edit"
+      v-model="editedText"
+      ref="input"
+      v-on:blur="finishEditing"
+      v-on:keypress.enter="finishEditing"
+      v-on:keyup.esc="cancelEditing"
+    />
   </li>
 </template>
 
@@ -22,12 +29,27 @@ export default {
       completed: Boolean,
     },
   },
-  data: () => {
+  data: function () {
     return {
+      editedText: this.todo.text,
       editing: false,
     };
   },
   methods: {
+    finishEditing: function () {
+      const trimmedText = this.editedText.trim();
+      if (trimmedText.length > 0) {
+        this.todo.text = trimmedText;
+        this.editedText = trimmedText;
+        this.editing = false;
+      } else {
+        this.$emit("destroy");
+      }
+    },
+    cancelEditing: function () {
+      this.editedText = this.todo.text;
+      this.editing = false;
+    },
     startEditing: function () {
       this.editing = true;
       this.$nextTick(() => {
