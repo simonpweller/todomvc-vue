1e4d105065f245d82e4e00542457a5d97c63282f
Simon Weller <s.meierbeck@gmail.com>
2020-06-17 22:42:39 +0200

    Enter editing mode

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,5 +1,5 @@
 <template>
-  <li :class="{ completed: todo.completed }">
+  <li :class="{ completed: todo.completed, editing }">
     <div class="view">
       <input
         class="toggle"
@@ -7,10 +7,10 @@
         :checked="todo.completed"
         v-on:change="$emit('toggle')"
       />
-      <label>{{ todo.text }}</label>
+      <label v-on:dblclick="startEditing">{{ todo.text }}</label>
       <button class="destroy" v-on:click="$emit('destroy')"></button>
     </div>
-    <input class="edit" :value="todo.text" />
+    <input class="edit" :value="todo.text" ref="input" />
   </li>
 </template>
 
@@ -20,6 +20,19 @@ export default {
     todo: {
       text: String,
       completed: Boolean,
+    },
+  },
+  data: () => {
+    return {
+      editing: false,
+    };
+  },
+  methods: {
+    startEditing: function () {
+      this.editing = true;
+      this.$nextTick(() => {
+        this.$refs.input.focus();
+      });
     },
   },
 };
